82a256cdc8d70a2a3a236fe5f93080ddcc6c4c6d
Gilver <rockgrub@disroot.org>
2026-01-03 23:33:47 -0600

    feat(find-develinfo.sh): Find .cmake files and modules (#79)

--- find-develinfo.sh
+++ find-develinfo.sh
@@ -10,6 +10,10 @@ for f in \
 	$buildroot/$_libdir/*.so \
 	$buildroot/$_libdir/pkgconfig/*.pc \
 	$buildroot//usr/share/gir-1.0/*.gir \
+	$buildroot/lib/cmake/* \
+	$buildroot/lib64/cmake/* \
+	$buildroot//usr/share/cmake/Modules/* \
+	$buildroot//usr/share/*/cmake/* \
 	; do
 	if grep -q '*' <<< "$f"; then continue; fi
 	if grep -q '/\.' <<< "$f"; then continue; fi
