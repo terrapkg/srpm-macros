079634d7061f6df2d1e633706b7a5f91a9a96d23
Gilver <rockgrub@disroot.org>
2026-01-04 12:49:14 -0600

    fix(find-develinfo.sh): Find command for .cmake files (#80)
    * fix(find-develinfo.sh) Ignore /usr/share/*/cmake
    
    This is WAY too greedy and matches doc files and far too many directories but I can't find a way to limit what it searches for (even `find` doesn't work correctly here).
    
    * fix: Horrible find command
    
    * chore: -and

--- find-develinfo.sh
+++ find-develinfo.sh
@@ -12,8 +12,7 @@ for f in \
 	$buildroot//usr/share/gir-1.0/*.gir \
 	$buildroot/lib/cmake/* \
 	$buildroot/lib64/cmake/* \
-	$buildroot//usr/share/cmake/Modules/* \
-	$buildroot//usr/share/*/cmake/* \
+	$(find $buildroot/usr/share -name "*.cmake" -type f -not -path "$buildroot/usr/share/doc/*" -and -not -path "$buildroot/usr/share/license/*") \
 	; do
 	if grep -q '*' <<< "$f"; then continue; fi
 	if grep -q '/\.' <<< "$f"; then continue; fi
