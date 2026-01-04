12774f07a5a943139421670e8570c8ff7c14d447
Gilver <roachy@fyralabs.com>
2026-01-04 12:47:26 -0600

    chore: Actually not needed

--- find-develinfo.sh
+++ find-develinfo.sh
@@ -12,7 +12,7 @@ for f in \
 	$buildroot//usr/share/gir-1.0/*.gir \
 	$buildroot/lib/cmake/* \
 	$buildroot/lib64/cmake/* \
-	$(find $buildroot/usr/share -name "*.cmake" -type f -not -path "$buildroot/usr/share/doc/*" -and -not -path "$buildroot/usr/share/license/*" -and -not -path "$buildroot/usr/share/*/resources/app.asar.unpacked/*") \
+	$(find $buildroot/usr/share -name "*.cmake" -type f -not -path "$buildroot/usr/share/doc/*" -and -not -path "$buildroot/usr/share/license/*") \
 	; do
 	if grep -q '*' <<< "$f"; then continue; fi
 	if grep -q '/\.' <<< "$f"; then continue; fi
