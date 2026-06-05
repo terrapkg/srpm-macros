266c40e8cd275a7acfcc78e9bb8171f69c1adbd7
Gilver <roachy@fyralabs.com>
2026-06-05 11:21:19 -0500

    fix: Double glob open paths

--- find-develinfo.sh
+++ find-develinfo.sh
@@ -6,12 +6,12 @@ _includedir=$(rpm -E '%_includedir')
 _libdir=$(rpm -E '%_libdir')
 
 for f in \
-	$buildroot/$_includedir/* \
+	$buildroot/$_includedir/** \
 	$buildroot/$_libdir/*.so \
 	$buildroot/$_libdir/pkgconfig/*.pc \
 	$buildroot//usr/share/gir-1.0/*.gir \
-	$buildroot/lib/cmake/* \
-	$buildroot/lib64/cmake/* \
+	$buildroot/lib/cmake/** \
+	$buildroot/lib64/cmake/** \
 	$(find $buildroot/usr/share -name "*.cmake" -type f -not -path "$buildroot/usr/share/doc/*" -and -not -path "$buildroot/usr/share/license/*") \
 	; do
 	if grep -q '*' <<< "$f"; then continue; fi
